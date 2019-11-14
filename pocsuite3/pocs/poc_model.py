67210ca4873f7322320128b77747bb19749a5fd6
root <root@localhost.localdomain>
2019-11-14 09:50:24 +0800

    修改vulID的设置，增加result

--- pocsuite3/pocs/poc_model.py
+++ pocsuite3/pocs/poc_model.py
@@ -4,9 +4,9 @@ from pocsuite3.api import Output, POCBase, POC_CATEGORY, register_poc, requests,
 from pocsuite3.lib.utils import random_str
 
 class DemoPOC(POCBase):
-    vulID = 'SSVID-97207 CVE-2018-7600'  # ssvid
+    vulID = 'SSVID-97207 CVE-2018-7600'
     version = '1.0'
-    author = ['seebug', 'cz']
+    author = ['cz']
     vulDate = '2018-03-08'
     createDate = '2018-04-12'
     updateDate = '2018-04-13'
@@ -30,10 +30,31 @@ class DemoPOC(POCBase):
             output.fail()
         return output
 
+    def _options(self):
+        pass
+
     def _verify(self):
-        return super()._verify()
+        result = {}
+
+        # try:
+        #     if resp.status_code == 200 and 'DEADBEEF' in resp.text:
+        #         result['VerifyInfo'] = {}
+        #         result['VerifyInfo']['URL'] = self.url
+        #         result['VerifyInfo']['Postdata'] = payload
+        # except Exception as ex:
+        #     logger.error(str(ex))
+        return self.parse_output(result)
 
     def _attack(self):
-        return super()._attack()
+        result = {}
+
+        # try:
+        #     if resp.status_code == 200 and 'DEADBEEF' in resp.text:
+        #         result['VerifyInfo'] = {}
+        #         result['VerifyInfo']['URL'] = self.url
+        #         result['VerifyInfo']['Postdata'] = payload
+        # except Exception as ex:
+        #     logger.error(str(ex))
+        return self.parse_output(result)
     
 register_poc(DemoPOC)
